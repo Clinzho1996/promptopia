3716230c01635e0bb1ca37460aef97f2181bd7d8
Clinzho1996 <confidinho@yahoo.com>
2023-07-04 15:25:38 +0100

    new update

--- app/page.jsx
+++ app/page.jsx
@@ -1,19 +1,29 @@
+"use client";
 import Feed from "@components/Feed";
+import { useSession } from "next-auth/react";
 
 const Home = () => {
+  const { data: session } = useSession();
   return (
     <section className="w-full flex-center flex-col">
       <h1 className="head_text text-center">
         Discover & Share
         <br className="max-md:hidden" />
-        <span className="blue_gradient text-center ml-2">
+        <span className="orange_gradient text-center ml-2">
           AI-Powered Prompts
         </span>
       </h1>
-      <p className="desc text-center">
-        Promptopia is an open-source AI prompting tool for modern world to
-        discover, create and share creative prompts
-      </p>
+      {session?.user ? (
+        <p className="desc text-center">
+          Hi {session?.user.name}, Promptopia is an open-source AI prompting
+          tool for modern world to discover, create and share creative prompts{" "}
+        </p>
+      ) : (
+        <p className="desc text-center">
+          Promptopia is an open-source AI prompting tool for modern world to
+          discover, create and share creative prompts
+        </p>
+      )}
 
       {/* feed */}
       <Feed />
